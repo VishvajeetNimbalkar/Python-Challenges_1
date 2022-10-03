1a81f66411c366c79ba1b459874ce973d0c6719c
Vishvajeet Nimbalkar <80111619+VishvajeetNimbalkar@users.noreply.github.com>
2022-10-03 17:22:36 +0530

    Add files via upload

---
+++ Challenge_1.py
@@ -0,0 +1,123 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # 1. Why python is popular in data science?
+
+# In[27]:
+
+
+## Because of its ease of use and simple syntax which makes it easy to adaptfor people who do not have an engineering background.
+
+
+# # 2. print your name in python
+
+# In[2]:
+
+
+print('vishvajee')
+
+
+# # 3. Correct the following python line
+
+# In[3]:
+
+
+print('Hi')
+
+
+# # 4. fill the black in the following python line , replace ___ with python in-built function
+
+# In[4]:
+
+
+print('hi')
+
+
+# # 5. add single line comments in the follwing line
+
+# In[5]:
+
+
+##this is comment 
+
+
+# # 6. add multiple comments in the follwing line
+
+# In[10]:
+
+
+'''
+this is python ide
+nyou can write code and create markdwon using this ide
+nyou can store input and output as it is in this ide'''
+
+
+# # 7. Do the addition of any two numbers and display the output
+
+# In[11]:
+
+
+7+8
+
+
+# # 8. Correct the following python code
+
+# In[19]:
+
+
+my_first_number = 10
+
+
+# # 9. create new variable named- myvar and assign string value value
+
+# In[20]:
+
+
+_myvar= 'ram'
+
+
+# # 10. create new variable named- my_number and assign int value 17
+
+# In[21]:
+
+
+my_number=17
+
+
+# # 11. Display/print the sum of 5 + 10, using two variables: x and y.
+
+# In[22]:
+
+
+x = 5
+y = 10
+print(x+y)
+
+
+# # 12. Create a variable called z, assign a + b to it, and display the result.
+
+# In[23]:
+
+
+x=5
+y=10
+z=x+y
+print(z)
+
+
+# # 13. Insert the correct syntax to assign the same value to all three variables in one code line. and display the values of a , b and c
+
+# In[24]:
+
+
+a=2
+b=6
+c=3
+print(b,c,a)
+
+
+# In[ ]:
+
+
+
+
